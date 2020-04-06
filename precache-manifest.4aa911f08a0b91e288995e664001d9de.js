04232b65afd5ff7b20035c5e1f6154e7e533ab96
Christopher Wong <cwong51799@gmail.com>
2020-04-06 19:45:46 -0400

    Updates

---
+++ precache-manifest.4aa911f08a0b91e288995e664001d9de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4dcb3ce057a1ed52832b36546a2abf2",
+    "url": "/nx-voter-bot/index.html"
+  },
+  {
+    "revision": "197f7a8c742c7f22715e",
+    "url": "/nx-voter-bot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2035e716d823b2a6282",
+    "url": "/nx-voter-bot/static/js/2.327d5ab8.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/nx-voter-bot/static/js/2.327d5ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197f7a8c742c7f22715e",
+    "url": "/nx-voter-bot/static/js/main.a59bfb9e.chunk.js"
+  },
+  {
+    "revision": "d09208c3f86dd9915131",
+    "url": "/nx-voter-bot/static/js/runtime-main.8e9d35c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nx-voter-bot/static/media/logo.5d5d9eef.svg"
+  }
+]);
